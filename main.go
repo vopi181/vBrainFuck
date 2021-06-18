a2cc06f8352a2f6bb80e3f4b4451b7e73dfed351
= <dompace181@gmail.com>
2021-06-18 12:53:10 -0500

    info

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"os"
 
 	"github.com/vopi181/vBrainFuck/bf"
 )
@@ -15,9 +17,14 @@ var (
 func main() {
 	var iFile string
 	flag.StringVar(&iFile, "file", "test.bf", "input file")
+	var info bool
+	flag.BoolVar(&info, "info", false, "print info")
 	flag.Parse()
-	//fmt.Println("vBrainFuck - BF to C compiler")
-	//fmt.Println("Build Info: " + BuildTime + "!")
-	//fmt.Println("working on " + iFile)
+	if info {
+		fmt.Println("vBrainFuck - BF to C compiler")
+		fmt.Println("Build Info: " + CommitHash + " @ " + BuildTime)
+		os.Exit(0)
+	}
+
 	bf.CompileFile(iFile)
 }
